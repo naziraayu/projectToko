8d97c8abb319f6d4dc7211fca9675b524caecd22
naziraayu <naziraayu2003@gmail.com>
2023-11-09 09:26:45 +0700

    Jeje
    Perubahan Ke-12

--- stokEtalase/stokEtalase.js
+++ stokEtalase/stokEtalase.js
@@ -4,7 +4,7 @@ const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const carouselChildrens = [...carousel.children];
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
-// Get the number of cards that can fit in the carousel at once
+// Get the number of cards that can fit in the carousel at once 
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 // Insert copies of the last few cards to beginning of carousel for infinite scrolling
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
