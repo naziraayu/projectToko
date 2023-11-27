e1b4df374ee4c2f2871d349a90721f63fe9c4b81
naziraayu <naziraayu2003@gmail.com>
2023-11-27 10:37:42 +0700

    Jeje
    Perubahan ke 29 (Perubahan date)

--- login/login.php
+++ login/login.php
@@ -12,7 +12,7 @@ if (isset($_POST['btnlogin'])) {
           $num=mysqli_num_rows($result);
           if ($num != 0) {
             $row=mysqli_fetch_assoc($result);
-            $id_user= $row['id_user']; 
+            $id_user= $row['id_user'];  
             $akses=$row['akses'];
             $telp=$row['no_telepon'];
             $pw=$row['password'];
